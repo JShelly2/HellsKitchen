cf9419f7c7c54cffe4b3a7a5578b0494092d0de9
jshelly2 <jshelly2@dtcc.edu>
2016-01-21 14:29:33 -0500

    Preliminary Version

--- src/p1/Main.java
+++ src/p1/Main.java
@@ -1,9 +1,26 @@
 package p1;
 
+import java.util.Scanner;
+/*
+Joshua Shelly
+700622764
+Demo menue 0.1
+1/21/16
+CSC164-402
+ */
 public class Main {
-
     public static void main(String[] Jagger) {
-        
-	// write your code here
+        Scanner input = new Scanner(System.in);
+        char option;
+        System.out.println("Welcome to Hell's Kitchen");
+        do{
+            System.out.println("(H)am \n(B)eef \n(S)alami \n(T)una \n(D)one");
+            String choice = input.next();
+            choice = choice.toUpperCase();
+            option = choice.charAt(0);
+            System.out.println("Choice is " + choice + " and option is " + option);
+        } while (option != 'D');
+        System.out.println("Thanks for ordering \n Prick.");
+        // write your code here
     }
 }
